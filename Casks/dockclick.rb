47fb9dd563eaaec5e6c8a86b20dbc493b587d04a
Sayed Hadi Hashemi <SayedHadiHashemi@gmail.com>
2026-03-28 19:14:07 -0700

    Add DockClick cask v1.2

---
+++ Casks/dockclick.rb
@@ -0,0 +1,17 @@
+cask "dockclick" do
+  version "1.2"
+  sha256 "52e72b815872f1421f07c115b4dcacf7611349c31d806f2c84eba452aa561c36"
+
+  url "https://github.com/xldrx/mac-dock-click/releases/download/v#{version}/DockClick-#{version}.zip"
+  name "DockClick"
+  desc "Open new windows by Cmd+clicking Dock icons"
+  homepage "https://github.com/xldrx/mac-dock-click"
+
+  depends_on macos: ">= :monterey"
+
+  app "DockClick.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.example.dockclick.plist",
+  ]
+end
